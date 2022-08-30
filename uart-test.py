9dd7cc4a48f1bcc1c6b23bb69bc18114c99fc563
HarindiPerera <104550460+HarindiPerera@users.noreply.github.com>
2022-08-30 16:36:01 +1000

    UART init fixed
    created a UART object and then initialised the bus

--- uart-test.py
+++ uart-test.py
@@ -1,10 +1,11 @@
 from machine import UART
 
-uart = UART(baudrate = 115200, bits=8, parity=None, stop=1, tx=1, rx=3)
+uart = UART(1,115200)
+uart.init(baudrate = 115200, bits=8, parity=None, stop=1, tx=1, rx=3)
 
 reply = b'\x01\x00'
 
-while true:
+while True:
     msg = uart.read()
 
     if msg:
